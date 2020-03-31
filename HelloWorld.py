48226d79c7e67c1ccd7e9e28e0b3d7376fb71fe7
Camille Brennan <camillebear@gmail.com>
2020-03-30 18:45:34 -0700

    1-6

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,25 @@
-print('snakes!')
-print('oranges:)')
-print('mail')
+name = input("enter your name : ")
+print ('hello '+ name )
+x = 10 
+print (x)
+n = int(input("enter a number : "))
+if n < 10:
+    print('Your number is less than 10!')
+elif n > 10: 
+    print('Your number in greater than 10!')
+elif n == 10: 
+    print('Your number is equal to 10!')
+p = int(input("enter a number : "))
+if p% 2 == 1: 
+    print('This number is odd.')
+else:
+    print('This number is even.')
+
+food = ['chocolate', 'water', 'juice'] 
+
+for index in food:
+    print(index)
+numbers = []
+for x in range(0,9):
+    numbers.append(x)
+print(numbers)
